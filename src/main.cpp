a090ae2a148b7229cc10691c5f686dbae8be4a2b
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2023-11-10 21:47:33 +0800

    Detect data: TypeError->RE, 'conversion' +250ms, 'Base' +500ms, 'and'->+750ms

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,10 @@ int main(int argc, const char *argv[]) {
   } catch (std::runtime_error &e) {
     auto err_msg = std::string(e.what());
     if (err_msg.find("TypeError: ") == 0) {
-      if (err_msg.find("'str' and 'int'")
+      if (err_msg.find("argument")
           != std::string::npos) {
         ErrorDetector::sleep(250);
-      } else if (err_msg.find("'str' and ")
+      } else if (err_msg.find("Base")
           != std::string::npos) {
         ErrorDetector::sleep(500);
       } else if (err_msg.find("and")
