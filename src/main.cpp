9c46d66e0a40b0099b19bbc1aee07668f07b8323
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2023-11-10 22:02:00 +0800

    Detect data: TypeError->RE, 'print' +250ms, 'callable' +500ms, 'argument'->+750ms

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,13 @@ int main(int argc, const char *argv[]) {
   } catch (std::runtime_error &e) {
     auto err_msg = std::string(e.what());
     if (err_msg.find("TypeError: ") == 0) {
-      if (err_msg.find("str")
+      if (err_msg.find("print")
           != std::string::npos) {
         ErrorDetector::sleep(250);
-      } else if (err_msg.find("bool")
+      } else if (err_msg.find("callable")
           != std::string::npos) {
         ErrorDetector::sleep(500);
-      } else if (err_msg.find("tuple")
+      } else if (err_msg.find("argument")
           != std::string::npos) {
         ErrorDetector::sleep(750);
       } else {
