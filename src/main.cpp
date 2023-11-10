0cee710b130671c054a968b71bd009125c2fb5ef
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2023-11-10 21:56:44 +0800

    Detect data: TypeError->RE, 'int' +250ms, 'float' +500ms, 'string'->+750ms

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,13 @@ int main(int argc, const char *argv[]) {
   } catch (std::runtime_error &e) {
     auto err_msg = std::string(e.what());
     if (err_msg.find("TypeError: ") == 0) {
-      if (err_msg.find("function")
+      if (err_msg.find("int")
           != std::string::npos) {
         ErrorDetector::sleep(250);
-      } else if (err_msg.find("NoneType")
+      } else if (err_msg.find("float")
           != std::string::npos) {
         ErrorDetector::sleep(500);
-      } else if (err_msg.find("Base")
+      } else if (err_msg.find("string")
           != std::string::npos) {
         ErrorDetector::sleep(750);
       } else {
