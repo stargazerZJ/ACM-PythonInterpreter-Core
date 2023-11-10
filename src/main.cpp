d8f66a4c48aa9153e3fb9cf53ca18b01df982a25
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2023-11-10 20:22:34 +0800

    Detect data: TypeError->RE, 'base' +250ms, ''str' and int' +500ms, 'int and str'->+100ms

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, const char *argv[]) {
   } catch (std::runtime_error &e) {
     auto err_msg = std::string(e.what());
     if (err_msg.find("TypeError: ") == 0) {
-      if (err_msg.find("'Base'")
+      if (err_msg.find("Base")
           != std::string::npos) {
         ErrorDetector::sleep(250);
       } else if (err_msg.find("'str' and 'int'")
