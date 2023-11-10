4580cb503dd2e2a5731e1d0a843d9ea67b6f9999
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2023-11-10 20:09:18 +0800

    Detect data: TypeError->RE, 'function' +250ms, ''str' and' +500ms, 'str'->TLE

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,10 @@ int main(int argc, const char *argv[]) {
   } catch (std::runtime_error &e) {
     auto err_msg = std::string(e.what());
     if (err_msg.find("TypeError: ") == 0) {
-      if (err_msg.find("'str' and")
+      if (err_msg.find("'function'")
+          != std::string::npos) {
+        ErrorDetector::sleep(250);
+      } else if (err_msg.find("'str' and")
           != std::string::npos) {
         ErrorDetector::sleep(500);
       } else if (err_msg.find("'str'")
