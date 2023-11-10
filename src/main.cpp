13c9e611de0c09b245a2b277fd73a573ee373c80
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2023-11-10 20:25:55 +0800

    Detect data: TypeError->RE, 'str+str' +250ms, ''str' and tuple' +500ms, 'tuple and str'->+100ms

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,13 @@ int main(int argc, const char *argv[]) {
   } catch (std::runtime_error &e) {
     auto err_msg = std::string(e.what());
     if (err_msg.find("TypeError: ") == 0) {
-      if (err_msg.find("Base")
+      if (err_msg.find("'str' and 'str'")
           != std::string::npos) {
         ErrorDetector::sleep(250);
-      } else if (err_msg.find("'str' and 'int'")
+      } else if (err_msg.find("'str' and 'tuple'")
           != std::string::npos) {
         ErrorDetector::sleep(500);
-      } else if (err_msg.find("'int' and 'str'")
+      } else if (err_msg.find("'tuple' and 'str'")
           != std::string::npos) {
         ErrorDetector::sleep(100);
       } else {
